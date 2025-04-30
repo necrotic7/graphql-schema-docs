24a35f0ca85d5242441d3a87c3d80bcc4df7847d
ZivWu <ziv.wu@clinico.com.tw>
2025-04-30 18:03:42 +0800

    fix: 修正index格式

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
-import path from 'path'
-import * as gqlmd from './utils/gqlmd'
-import { getAllSchemaFilePath } from './utils/files'
-import * as voyager from './utils/voyager'
-import { getLogger } from './utils/logger'
+import path from 'path';
+import * as gqlmd from './utils/gqlmd';
+import { getAllSchemaFilePath } from './utils/files';
+import * as voyager from './utils/voyager';
+import { getLogger } from './utils/logger';
 
-const TAG = '[Main]'
+const TAG = '[Main]';
 
-;(async function () {
-    const basePath = path.join(__dirname, 'project')
+(async function () {
+    const basePath = path.join(__dirname, 'project');
     // 取得所有schema
-    const schemaFiles = await getAllSchemaFilePath(basePath)
+    const schemaFiles = await getAllSchemaFilePath(basePath);
 
     await Promise.all(
         schemaFiles.map(async ({ dirPath, files }) => {
@@ -18,12 +18,12 @@ const TAG = '[Main]'
                 gqlmd.docRender(dirPath, files),
                 // 建立voyager
                 voyager.builder(dirPath, files),
-            ])
+            ]);
         }),
-    )
+    );
     // 啟動voyager
-    voyager.start()
+    voyager.start();
 })().catch((err) => {
-    getLogger().error(TAG, err)
-    process.exit(1)
-})
+    getLogger().error(TAG, err);
+    process.exit(1);
+});
